6b7b7e197b45cef095980d0128d1f3453e642c89
cdsouza <cheryldsouza10@gmail.com>
2024-01-31 12:13:11 +0530

    added env variables

--- src/dbConnector.js
+++ src/dbConnector.js
@@ -1,12 +1,13 @@
 const fp = require("fastify-plugin");
 const fastifyPostgres = require("fastify-postgres");
+require("dotenv").config();
 
 module.exports = fp(async (fastify, options) => {
   fastify.register(fastifyPostgres, {
-    host: "localhost",
-    port: "5432",
-    database: "authenticate",
-    user: "postgres",
-    password: "password",
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
   });
 });
